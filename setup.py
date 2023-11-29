82ede4d8ced98c4e6f219abdde3a45783f5e1a39
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2023-11-29 14:53:46 -0700

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Mining users from github and then performing the analysis on the users data',
+    author='Venkata, Shashi',
+    license='MIT',
+)
