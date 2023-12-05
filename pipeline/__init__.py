b5558a08f0c8a8b0f8fe8b6ba09c9e5cd7595936
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2023-12-05 14:38:52 -0700

    updated the github pipeline file

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -17,9 +17,12 @@ class PipelineConfiguration(object):
     def initiate_data_ingestion(self) -> DataIngestionArtifact:
         try:
             logging.info(f"Initiating data ingestion")
+            print(f"Initiating data ingestion")
             data_ingestion_configuration = self.configuration.get_data_ingestion_configuration()
+            print(f"Data ingestion configuration completed successfully........!")
             data_ingestion_component = DataIngestionComponent(data_ingestion_configuration)
             data_ingestion_component.github_users_data_extraction_and_writing_to_csv()
+            print(f"Data ingestion completed successfully")
             logging.info(f"Data ingestion completed successfully")
         except Exception as e:
             raise CustomException(e, sys)
