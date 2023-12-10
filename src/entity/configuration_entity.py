64c31a9ee91c14529f597f2fa28d7f8eb60872f7
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2023-12-10 16:29:08 -0700

    updated data transformation Configuration entity

--- src/entity/configuration_entity.py
+++ src/entity/configuration_entity.py
@@ -3,5 +3,5 @@ from collections import namedtuple
 DataIngestionConfiguration = namedtuple(
     'DataIngestionConfiguration', [ 'source_data_directory', 'ingested_directory', 'ingested_train_directory','ingested_test_directory'])
 
-
+DataTransformationConfiguration = namedtuple('DataTransformationConfiguration', [ 'preprocessed_data_directory'])
 TrainingPipelineConfiguration = namedtuple('TrainingPipelineConfiguration', ['artifact_directory'])
